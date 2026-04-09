1921a9e8366af906648af6d151ff5f4180d09cbc
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-09 22:42:18 +0530

    create and start server

---
+++ server_expjs/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+// express() // creates server // but not started
+
+const app = express()
+
+app.listen(3000) // method to start server ( 3000 = prot number )
