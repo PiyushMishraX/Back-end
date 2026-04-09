214e5ee00fc673b86c6590b3d96dbf7bb6d4265c
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-09 23:11:51 +0530

    addin pages

--- server_expjs/server.js
+++ server_expjs/server.js
@@ -1,9 +1,28 @@
-const express = require('express');
+// const express = require('express');
 
-// express() // creates server // but not started
+// // express() // creates server // but not started
+
+// const app = express()
+
+// app.listen(3000) // method to start server ( 3000 = prot number ) // now use node server.js to use //if chartat in terminal is stuck in place then server is running
+// the server is running locally so we can access using browser at // listen create/start a server that can crun in backend and node file.js executes it
+// localhost:port -> localhost:3000
+
+// for now it gives error becuase we haven't program  it for use to use
+
+
+const express = require('express');
 
 const app = express()
 
-app.listen(3000) // method to start server ( 3000 = prot number ) // now use node server.js to use //if chartat in terminal is stuck in place then server is running
-// the server is running locally so we can access using browser at // listen create/start a server that can crun in backend and node file.js executes it
-// localhost:port -> localhost:3000
+app.get("/", (req,res)=>{
+    res.send("Hello World")
+}) // this code does't appear in alredy runing server so close it ctrl+c then restart it node server.js
+// here we programed the server to respond for user request ( here it is just opening the server and opening site) 
+
+
+app.get("/about", (req,res)=>{ 
+    res.send("About Page") // check at site/about // open after restarting server
+})
+
+app.listen(3000) 
