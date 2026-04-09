475400e1c6c1b1df05c2fe6dacf94c7cda55243a
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-09 22:55:02 +0530

    server in backend and server at execution

---
+++ server_expjs/index.js
@@ -0,0 +1,1 @@
+console.log(2+3); //  node index.js  , server start, execute and shut down but with listen method it runs on background
--- server_expjs/server.js
+++ server_expjs/server.js
@@ -4,4 +4,6 @@ const express = require('express');
 
 const app = express()
 
-app.listen(3000) // method to start server ( 3000 = prot number )
+app.listen(3000) // method to start server ( 3000 = prot number ) // now use node server.js to use //if chartat in terminal is stuck in place then server is running
+// the server is running locally so we can access using browser at // listen create/start a server that can crun in backend and node file.js executes it
+// localhost:port -> localhost:3000
