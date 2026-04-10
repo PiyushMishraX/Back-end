6c0c63ec2343f8a24012a1b1c4ff408f4c76059a
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-10 18:04:13 +0530

    post api full

--- api_restapi/notes_api/src/app.js
+++ api_restapi/notes_api/src/app.js
@@ -4,6 +4,7 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json()) // create body data redable for express
 
 const notes = []
 
@@ -13,9 +14,16 @@ const notes = []
 //  POST /notes
 app.post('/notes', (req, res)=>{ // req mai data aayega 
 
-    console.log(req.body); // data qill come in req.body
-    
+    // console.log(req.body); // data qill come in req.body
+    // we have to sent data from frontend , but to sent data from browser qw need to create a ui , instead for testing we use POSTMAN
+    // output is undefined so we have to use middleware ( future topic)
 
+    notes.push(req.body);
+
+    // 201 succes creating new resourc 
+    res.status(201).json({
+        message: "note created successfully" // see in postman response
+    })
 }) 
 
 module.exports = app // export server after creation // expoting instance of server
