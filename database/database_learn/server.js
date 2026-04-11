252909d38d6acca94c656884e8a0dfce5faf8711
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-11 16:39:11 +0530

    solving dns problem)

--- database/database_learn/server.js
+++ database/database_learn/server.js
@@ -1,7 +1,25 @@
-const app = require('/src/app')
+const app = require("./src/app")
+const connectDB = require("./src/db/db")
 
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
-    
-})
+const dns = require("dns")
+
+dns.setServers(["1.1.1.1","8.8.8.8"]);
+
+connectDB();
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
+});
+
+
+// connectDB()
+//   .then(() => {
+//     app.listen(3000, () => {
+//       console.log("Server running on port 3000");
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Failed to connect to DB:", err);
+//     process.exit(1); // Stop the process if DB connection fails
+//   });
