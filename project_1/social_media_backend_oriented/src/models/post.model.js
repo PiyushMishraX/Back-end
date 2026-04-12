06cd498686a8cb5d7418948f5d517f64fa055212
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-12 16:11:57 +0530

    creating post model

---
+++ project_1/social_media_backend_oriented/src/models/post.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+
+const postSchema = new mongoose.Schema({
+    image: String,
+    caption: String,
+})
+
+const postModel = mongoose.model("post", postSchema)
+
+
+
+/* 
+    post = {
+    image: String,
+    caption: String,
+    }
+
+    //in future 
+    user ={
+    name:String,
+    email: string,
+    password: String,
+    posts:[post1,post2,post3]
+    }
+
+    same type of data get sored in al collection
+    different data are stored in different collection and
+    ("post", postSchema) ---> post is name of the collection
+
+
+
+*/
