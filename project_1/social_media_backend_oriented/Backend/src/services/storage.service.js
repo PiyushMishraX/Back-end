9752a5c9f0c84d798ddc87511c016824bb132f15
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-12 20:54:37 +0530

    using env

--- project_1/social_media_backend_oriented/Backend/src/services/storage.service.js
+++ project_1/social_media_backend_oriented/Backend/src/services/storage.service.js
@@ -2,7 +2,8 @@ const ImageKit =  require("@imagekit/nodejs")
 
 const imagekit = new ImageKit({
 
-    privateKey: "key"
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+    // with private key a person can upload and delete the things on the account and the person want to use imagekit have to pay for the sotrage the key shoul din in env
 }) 
 
 async function uploadFile(buffer) {
