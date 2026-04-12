2d712d37355825ba886c1776afd3ad99f7ded326
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-13 00:08:29 +0530

    add navigation of feed page ( small full stack mern application created)

--- project_1/social_media_backend_oriented/Frontend/src/pages/CreatePost.jsx
+++ project_1/social_media_backend_oriented/Frontend/src/pages/CreatePost.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import axios from 'axios'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
+
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) =>{
     e.preventDefault()
 
     const formData = new FormData(e.target)
+    // The FormData interface provides a way to construct a set of key/value pairs representing form fields and their values, which can be sent using the fetch(), XMLHttpRequest.send() or navigator.sendBeacon() methods. It uses the same format a form would use if the encoding type were set to "multipart/form-data".
+
+    // the main work here is done by formData which transfers the full big res to form-data , key/value pair and send the data like postman
 
     // post to add the / upload the data
     axios.post("http://localhost:3000/create-post", formData)
     .then((res)=>{
 
-      console.log(res);
+      // console.log(res);
+
+      navigate("/feed")
       
 
       // alert("Post created suucessfully")
