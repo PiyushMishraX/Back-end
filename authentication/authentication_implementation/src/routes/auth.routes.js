045b29a3761bc71ae52db657e1b25871c54665d1
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-14 18:30:07 +0530

    server accessing cookies at requests

--- authentication/authentication_implementation/src/routes/auth.routes.js
+++ authentication/authentication_implementation/src/routes/auth.routes.js
@@ -6,4 +6,16 @@ const router = express.Router();
 // POST /api/auth/register ---> testing  in postman use this  
 router.post("/register",  authController.registerUser)
 
+
+
+// seeing the cookies being send to server at every request 
+router.get("/test", (req, res)=>{
+    console.log("Cookies: ",  req.cookies); // access cookies in browser through request. cookies 
+    res.json({
+        message: "Test route",
+        cookies: req.cookies
+    })
+    
+})
+
 module.exports = router;
