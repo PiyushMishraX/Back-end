b6eea684bd17fbf08c732f7a11f18c49a8dfc5c7
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-14 19:44:23 +0530

    add invalid token protection

--- authentication/authentication_implementation/src/routes/post.routes.js
+++ authentication/authentication_implementation/src/routes/post.routes.js
@@ -17,7 +17,21 @@ router.post("/create", (req,res)=>{
             message: "Unauthorized"
 
         })
-    } 
+    } // still the post can be created with wrong token
+    // token can be checked ( if it is created from our server or not ) using jsonwebtoken package ( same that creates)
+
+    
+    try{
+        jwt.verify(token, process.env.JWT_SECRET) // if server do not have created that token than jwt.verify throws an error and if error is catched than catch block handles it
+        
+    }catch(err){
+        return res.status(401).json({
+            message: "Token is invalid"
+        })
+    }
+
+    res.send("Post created successfully")
+    
 })
 
 module.exports = router;
