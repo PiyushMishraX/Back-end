4ce2009d06d4d1082415ad2de5540c235bcb30d1
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-14 19:55:34 +0530

    finding user from decoded vlaues

--- authentication/authentication_implementation/src/routes/post.routes.js
+++ authentication/authentication_implementation/src/routes/post.routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const userModel = require('../models/user.model');
 
 const router = express.Router()
 
-router.post("/create", (req,res)=>{
+router.post("/create", async (req,res)=>{
     // console.log(req.body)
 
     // console.log(req.cookies);
@@ -24,9 +25,17 @@ router.post("/create", (req,res)=>{
     try{
         // jwt.verify(token, process.env.JWT_SECRET) // if server do not have created that token than jwt.verify throws an error and if error is catched than catch block handles it
 
-        // if the token is real( correct ) than jwt.verify do not return an error nut returns the object which we encoded in token ( here the _id)
+        // if the token is real( correct ) than jwt.verify do not return an error nut returns the object which we encoded in token ( here the _id) 
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET) 
+        // console.log(decoded); // { _id , iat //token cration timing } // data given at time of token creation
+        
         const decoded = jwt.verify(token, process.env.JWT_SECRET) 
-        console.log(decoded); // { _id , iat //token cration timing }
+
+        const user = await userModel.findOne({
+            _id: decoded.id // returns the user with that specific if ( gives the data stored)
+        })
+
+        console.log(user);
         
         
     }catch(err){
