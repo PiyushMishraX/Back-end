f59afd31d9c23e80322ee82556da56fb03469cdc
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-14 18:21:01 +0530

    cookie use and cookie paser middleware

--- authentication/authentication_implementation/src/app.js
+++ authentication/authentication_implementation/src/app.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const authRoutes = require("./routes/auth.routes")
+const express = require("express");
+const authRoutes = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes) // have to use prefix -> /api/auth to ask api created with router
+app.use("/api/auth", authRoutes); // have to use prefix -> /api/auth to ask api created with router
 
 module.exports = app;
