9a1404300dffad28f63d65767eaa1da7fd485488
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-15 23:19:19 +0530

    authorize artists to create music

---
+++ spotify_backend_focused/src/controllers/music.controller.js
@@ -0,0 +1,27 @@
+const musicModel = require("../models/music.model")
+const jwt = require("jsonwebtoken")
+
+
+async function createMusic(req,res){
+
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({message: "unauthorized"})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(decoded.role !== "artist"){
+            // return res.status(403).json({message: "Forbidden"})
+            return res.status(403).json({message: "You don't have access to create an music"})
+        }
+        
+    } catch (err) {
+        return res.status(401).json({message: "Unathorized"})
+    }
+
+
+
+}
