95f50ca054909faf47fc65794a9c35cb62ee5dd3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-16 21:05:58 +0530

    middleware for token verification and authorization

--- spotify_backend_focused/src/middlewares/auth.middleware.js
+++ spotify_backend_focused/src/middlewares/auth.middleware.js
@@ -1,5 +1,28 @@
 const jwt = require("jsonwebtoken");
 
+// create middleware , 3 parameters are required
 async function authArtist(req, res , next){
+
+    const token = req.cookies.token;
     
+        if(!token){
+            return res.status(401).json({message: "unauthorized"})
+        }
+    
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    
+            if(decoded.role !== "artist"){
+                // return res.status(403).json({message: "Forbidden"})
+                return res.status(403).json({message: "You don't have access to create an music"})
+            }
+          
+        } catch (err) {
+
+        console.log(err);
+        
+
+        return res.status(401).json({message: "Unathorized"})
+        }
+
 }
