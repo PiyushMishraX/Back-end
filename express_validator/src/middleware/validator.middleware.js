8c535eab4f1924906a756b4268aeeb8fe183aee4
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-18 22:08:04 +0530

    validate email and password

--- express_validator/src/middleware/validator.middleware.js
+++ express_validator/src/middleware/validator.middleware.js
@@ -28,5 +28,15 @@ const registerUserValidationRUles = [
         .isLength({min:3, max: 20})
         .withMessage("Username must be between 3 and 20 characters"), 
         // if conditions not followed we will get error we will handle it in a function
-    validateResult // run when anything any rule fails
+    // validateResult // run when anything any rule fails
+
+    body("email")
+        .isEmail()
+        .withMessage("Invlaid email address"),
+
+    body("email")
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 characters long"),
+
+    validateResult // run when anything any rule fails / runs everytime and returns when req have any error generated above
 ]
