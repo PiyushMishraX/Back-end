bac46b9e350c22dd2984a55ee255b9e2628c8920
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-18 21:39:34 +0530

    creating testcase

--- miscellaneous/src/test/_app.test.js
+++ miscellaneous/src/test/_app.test.js
@@ -1,3 +1,14 @@
 const request = require('express')
+const app = require('../app')
 
-describe()
+describe("GET /", ()=>{ // write api to test and testcases in callback
+    it("should return 200 OK", async ()=>{ // for simple req no data
+
+        const res = await request(app).get("/");
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({message: "Hello!"});
+
+    })
+
+})
