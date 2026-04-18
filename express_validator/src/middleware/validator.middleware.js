9cb7adc6320d1dbcaf37031f3a128de416338623
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-18 21:56:30 +0530

    validate username

--- express_validator/src/middleware/validator.middleware.js
+++ express_validator/src/middleware/validator.middleware.js
@@ -1,2 +1,17 @@
-const { body, param, query } = require('express-validator');
+// const { body, param, query } = require('express-validator');
 //  body, param, query -> data can come in this
+
+// const { check } = require('express-validator');
+// if do not knpw where the dat ais coming on
+
+// we go for specifics
+const { body, validationResult } = require('express-validator');
+
+const registerUserValidationRUles = [
+
+    body("username")
+        .isString()
+        .withMessage("Username must be string")
+        .isLength({min:3, max: 20})
+        .withMessage("Username must be between 3 and 20 characters"),
+]
